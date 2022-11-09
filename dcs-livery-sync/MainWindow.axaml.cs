9bca8478cf1f62923e1519e65e98e5dec1eadccb
Matthew J. Kosmoski <mkosmo@gmail.com>
2022-11-08 18:16:11 -0600

    Initial commit.

---
+++ dcs-livery-sync/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace dcs_livery_sync
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
